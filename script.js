ac558309f74ea8f8d51c7c9055f7a34e2c1ae41a
yousef <yousefsameh24@gmail.com>
2023-04-12 15:51:25 +0200

    handle hover over cells

--- script.js
+++ script.js
@@ -2,80 +2,114 @@ const grid16 = document.getElementById('16x16');
 const grid32 = document.getElementById('32x32');
 const grid64 = document.getElementById('64x64');
 
+const clear = document.getElementById('clear');
+
 const container = document.getElementById('grid-container');
 
-let isGrid16 = false;
+let isGrid16 = true;
 let isGrid32 = false;
 let isGrid64 = false;
 
 let gridSize = 0;
 
+//on default grid is set to 16x16
+for(let i = 0; i < 16; i++) {
+    let row = document.createElement('div');
+    row.classList.add('grid-row');
+    container.appendChild(row);
+    for(let j = 0; j < 16; j++) {
+        let cell = document.createElement('div');
+        cell.classList.add('grid-cell');
+        row.appendChild(cell);
+    }
+}
+
+grid16.disabled = true;
+clear.disabled = true;
+
+//handle grid size buttons
 grid16.addEventListener('click', () => {
     isGrid16 = true;
     gridSize = 16;
-
-    document.getElementById("clear").disabled = false;
-    if(isGrid32 || isGrid64){
-        document.getElementById("32x32").disabled = false;
-        document.getElementById("64x64").disabled = false;
+    if(isGrid32 || isGrid64 ){
+        grid32.disabled = false;
+        grid64.disabled = false;
         container.innerHTML = '';
     }
 
     for(let i = 0; i < 16; i++) {
-        let grid = document.createElement('div');
-        grid.classList.add('grid-square');
-        document.getElementById("grid-container").appendChild(grid);
+        let row = document.createElement('div');
+        row.classList.add('grid-row');
+        container.appendChild(row);
+        for(let j = 0; j < 16; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('grid-cell');
+            row.appendChild(cell);
+        }
     }
 
-    document.getElementById("16x16").disabled = true;
+    grid16.disabled = true;
 })
 
 grid32.addEventListener('click', () => {
     isGrid32 = true;
     gridSize = 32;
 
-    document.getElementById("clear").disabled = false;
     if(isGrid16 || isGrid64){
-        document.getElementById("16x16").disabled = false;
-        document.getElementById("64x64").disabled = false;
+        grid16.disabled = false;
+        grid64.disabled = false;
         container.innerHTML = '';
     }
     for(let i = 0; i < 32; i++) {
-        let grid = document.createElement('div');
-        grid.classList.add('grid-square');
-        document.getElementById("grid-container").appendChild(grid);
+        let row = document.createElement('div');
+        row.classList.add('grid-row');
+        container.appendChild(row);
+        for(let j = 0; j < 32; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('grid-cell');
+            row.appendChild(cell);
+        }
     }
     
-    document.getElementById("32x32").disabled = true;
+    grid32.disabled = true;
 })
 
 grid64.addEventListener('click', () => {
     isGrid64 = true;
     gridSize = 64;
     
-    document.getElementById("clear").disabled = false;
     if(isGrid16 || isGrid32){
-        document.getElementById("16x16").disabled = false;
-        document.getElementById("32x32").disabled = false;
+        grid16.disabled = false;
+        grid32.disabled = false;
         container.innerHTML = '';
     }
 
     for(let i = 0; i < 64; i++) {
-        let grid = document.createElement('div');
-        grid.classList.add('grid-square');
-        document.getElementById("grid-container").appendChild(grid);
+        let row = document.createElement('div');
+        row.classList.add('grid-row');
+        container.appendChild(row);
+        for(let j = 0; j < 64; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('grid-cell');
+            row.appendChild(cell);
+        }
     }
     
-    document.getElementById("64x64").disabled = true;
+    grid64.disabled = true;
 })
 
-const clear = document.getElementById('clear');
+//handle clear button
 clear.addEventListener('click', () => {
     container.innerHTML = '';
     for(let i = 0; i < gridSize; i++) {
-        let grid = document.createElement('div');
-        grid.classList.add('grid-clear');
-        document.getElementById("grid-container").appendChild(grid);
+        let row = document.createElement('div');
+        row.classList.add('grid-row');
+        container.appendChild(row);
+        for(let j = 0; j < gridSize; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('grid-cell');
+            row.appendChild(cell);
+        }
     }
-    document.getElementById("clear").disabled = true;
+    clear.disabled = true;
 })
